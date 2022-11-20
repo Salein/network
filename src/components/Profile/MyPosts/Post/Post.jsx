52a6b932900b15ea0c36f1b446e58a4cffb956a2
Sallein <saleinkyl@gmail.com>
2022-11-20 23:29:06 +0300

    добавлены Post\MyPosts и немного причесаны

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = () => {
 
   return (
     <div className={s.item}>
-      <img src="https://avatarfiles.alphacoders.com/922/92251.jpg" />
+      <img src="https://avatarfiles.alphacoders.com/922/92251.jpg" alt="" />
       Posts 1
       <div>
         <span>Like</span>
