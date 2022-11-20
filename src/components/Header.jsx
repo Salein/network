b0b6780c5dac4675d11bc25751889e77bc11ce9a
Sallein <saleinkyl@gmail.com>
2022-11-20 22:13:58 +0300

    добавление модулей и причесывание

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import s from './Header.module.css'
+
 
 const Header = () => {
 
-
     return(
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://i1.7fon.org/thumb/g577761.jpg" alt="" />
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import React from "react"
+import s from './Profile.module.css'
+
+
 
 const Profile = () => {
 
-
     return(
-        <div className='content'>
+        <div className={s.content}>
         <div>
-        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt=""/>
         </div>
         <div>
           ava
