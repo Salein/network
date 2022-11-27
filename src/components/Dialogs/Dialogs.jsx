3640b4a16247615990cf391b4e188bec0fe653bd
Sallein <saleinkyl@gmail.com>
2022-11-27 12:18:44 +0300

    created Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,40 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 import s from './Dialogs.module.css'
 
 
-const Dialogs = () => {
+export const DialogItem = () => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to='/dialogs/1'>awd</NavLink>
+        </div>
+    )
+}
 
-    return(
-        <div>
-            Dialogs
+const Dialogs = (props) => {
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <DialogItem />
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>awdawawfd</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>wafawfae</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>yukyuk</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>ry6rur6</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>gi</div>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>pe</div>
+            </div>
         </div>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,30 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 import s from './NavBar.module.css'
-
+import './NavBar.module.css'
 
 
 const NavBar = () => {
 
-    return(
-        <nav className={s.nav}>
-        <div className={`${s.item} ${s.active}`}>
-          <a href="/profile">Profile</a>
-        </div>
-        <div className={s.item}>
-        <a href="dialogs">Messages</a>
-        </div>
-        <div className={s.item}>
-        <a href="news">News</a>
-        </div>
-        <div className={s.item}>
-        <a href="musik">Musik</a>
-        </div>
-        <div className={s.item}>
-        <a href="settings">Settings</a>
-        </div>
-      </nav>
-    )
+  return (
+    <nav className={s.nav}>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/profile" className={ navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/dialogs" className={ navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/news" className={ navData => navData.isActive ? s.active : s.item }>News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/musik" className={ navData => navData.isActive ? s.active : s.item }>Musik</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/settings" className={ navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
