2f08481400f64c6cea1be0023a4451131b046d83
Sallein <saleinkyl@gmail.com>
2023-03-04 18:09:24 +0300

    re: messages-reduser

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,8 +6,8 @@ import Message from "./Message/Message"
 
 const Dialogs = (props) => {
 
-    let dialogsElements = props.messagesPage.dialogs.map(d => <DialogItem name={d.name} id={d.id} />)
-    let messagesElements = props.messagesPage.messages.map(m => <Message message={m.message} />)
+    let dialogsElements = props.messagesPage.dialogs.map(d => <DialogItem name={d.name} id={d.id} key={d.id} />)
+    let messagesElements = props.messagesPage.messages.map(m => <Message message={m.message} key={m.id} />)
     let newMessageBody = props.messagesPage.newMessageBody
 
     let onSendMessage = () => {
--- src/redux/messages-reducer.js
+++ src/redux/messages-reducer.js
@@ -21,20 +21,17 @@ let initialState = {
 
 export const messagesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY: {
-      let stateCopy = {...state}
-      stateCopy.newMessageBody = [...state.newMessageBody]
-      stateCopy.newMessageBody = action.body;
-      return stateCopy;
-    }
-    case SEND_MESSAGE: {
-      let stateCopy = {...state}
-      stateCopy.newMessageBody = [...state.newMessageBody]
-      let body = stateCopy.newMessageBody;
-      stateCopy.newMessageBody = "";
-      stateCopy.messages.push({ id: 6, message: body });
-      return stateCopy;
-    }
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {...state,
+        newMessageBody: action.body}
+
+    case SEND_MESSAGE:
+      let body = state.newMessageBody;
+      return {
+        ...state,
+        newMessageBody: "",
+        messages: [...state.messages, { id: 6, message: body }]
+      }
     default:
       return state;
   }
